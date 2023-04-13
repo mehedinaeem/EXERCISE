518d00e65c8fa80e2764e76ab5f7eb4f80a2693d
mehedinaeem <mehedinaeem00@gmail.com>
2023-04-13 09:51:08 +0600

    first commit

---
+++ C/n_prime_number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,count=1,flag,i=2,j;
+    printf("Enter how many num u want to print:");
+    scanf("%d",&n);
+
+    while(count<=n)
+    {
+        flag=0;
+        for(j=2;j<=i/2;j++)
+        {
+            if(i%j==0)
+            {
+                flag++;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            printf("%d\t",i);
+            count++;
+        }
+        i++;
+    }
+}
