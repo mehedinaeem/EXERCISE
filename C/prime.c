f86cc8508d84b3067324fe032f0e62d2be9eb0de
mehedinaeem <mehedinaeem00@gmail.com>
2023-04-16 13:42:59 +0600

    ex

---
+++ C/prime.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int count;
+
+    for (int i = 2; i <= n; i++)
+    {
+        count=0;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+                {
+                    count++;
+                    break;
+                }
+        }
+        if(count==0)
+            printf("%d ",i);
+
+    }
+    return 0;
